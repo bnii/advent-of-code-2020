dc4d691364758b4e1d8480959fc05f3423708fe5
Istvan Ban <ban.istvan@dteam.hu>
2020-12-03 10:51:23 +0100

    day3/2

--- src/day3.clj
+++ src/day3.clj
@@ -4,7 +4,6 @@
 
 (defn slope-seq [s down-step right-step]
   (loop [ns (map cycle s) slope []]
-    (sc.api/spy)
     (if-let [re (seq (drop down-step ns))]
       (let [m (map #(drop right-step %) re)]
         (recur m (conj slope (ffirst m))))
@@ -14,3 +13,13 @@
   (count (filter #(= \# %) slopes)))
 
 (count-trees (slope-seq (load-input 3) 1 3))
+
+(let [terrain (load-input 3)
+      steps   [[1 1] [1 3] [1 5] [1 7] [2 1]]]
+  (->>
+    steps
+    (map #(apply slope-seq terrain %))
+    (map #(filter (fn [c] (= \# c)) %))
+    (map count)
+    (apply *)))
+
