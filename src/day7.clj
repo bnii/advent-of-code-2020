a522de5093000f4bb168e85e95cbaf6b8dcff51d
Istvan Ban <ban.istvan@dteam.hu>
2020-12-07 11:54:25 +0100

    day7/1

--- src/day7.clj
+++ src/day7.clj
@@ -1,1 +1,25 @@
-(ns day7)
+(ns day7
+  (:refer-clojure)
+  (:require [common :refer [load-input]]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as alg]))
+
+(def example-row "posh lavender bags contain 4 light teal bags, 4 wavy turquoise bags, 1 dim yellow bag.")
+
+(defn row->edge
+  "returning an sequence of edges of [contained-bag container-bag num-it-can-hold]"
+  [row]
+  (let [[_ container-bag contained-bags-string] (re-matches #"^(.*) bags? contain (.*)\.$" row)
+        contained-bags-splitted (clojure.string/split contained-bags-string #", ")]
+    (->> contained-bags-splitted
+         (map #(re-matches #"^(\d+) ([a-z ]+) bags?" %))
+         (map (fn [[_ occur bag]] [bag container-bag (Integer/parseInt occur)])))))
+
+(row->edge example-row)
+
+(def graph-init-directed-weighted (apply concat (map row->edge (remove #(re-matches #".*no other.*" %) (load-input 7)))))
+(def bag-containment-gaph (apply uber/digraph graph-init-directed-weighted))
+
+;;1
+(dec (count (alg/topsort bag-containment-gaph "shiny gold")))
+
--- test/day7_test.clj
+++ test/day7_test.clj
@@ -1,3 +1,11 @@
 (ns day7-test
   (:require [clojure.test :refer :all])
   (:require [day7 :refer [row->edge]]))
+
+(def example-row "posh lavender bags contain 4 light teal bags, 4 wavy turquoise bags, 1 dim yellow bag.")
+
+(deftest row->edge-test
+  (is (= (row->edge example-row)
+         [["light teal" "posh lavender" 4]
+          ["wavy turquoise" "posh lavender" 4]
+          ["dim yellow" "posh lavender" 1]])))
