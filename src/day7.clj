c857c01a1ceb129738fbb4809d7d2216fd2bff2d
Istvan Ban <ban.istvan@dteam.hu>
2020-12-07 13:39:15 +0100

    day7/2

--- src/day7.clj
+++ src/day7.clj
@@ -4,8 +4,6 @@
             [ubergraph.core :as uber]
             [ubergraph.alg :as alg]))
 
-(def example-row "posh lavender bags contain 4 light teal bags, 4 wavy turquoise bags, 1 dim yellow bag.")
-
 (defn row->edge
   "returning an sequence of edges of [contained-bag container-bag num-it-can-hold]"
   [row]
@@ -18,8 +16,20 @@
 (row->edge example-row)
 
 (def graph-init-directed-weighted (apply concat (map row->edge (remove #(re-matches #".*no other.*" %) (load-input 7)))))
-(def bag-containment-gaph (apply uber/digraph graph-init-directed-weighted))
+(def bag-containment-graph (apply uber/digraph graph-init-directed-weighted))
 
 ;;1
-(dec (count (alg/topsort bag-containment-gaph "shiny gold")))
+(dec (count (alg/topsort bag-containment-graph "shiny gold")))
+
+(def contained-count-by
+  (memoize (fn [node]
+             (let [predecs (uber/predecessors bag-containment-graph node)]
+               (->> predecs
+                    (map #(*
+                            (uber/weight bag-containment-graph % node)
+                            (inc (contained-count-by %))))
+                    (apply +))))))
+
+;;2
+(contained-count-by "shiny gold")
 
