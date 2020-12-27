e34a19d69d41e6e6bd7ff0bc3e921bb23009b5a1
Istvan Ban <ban.istvan@dteam.hu>
2020-12-27 15:11:54 +0100

    squash: slurp

--- src/common.clj
+++ src/common.clj
@@ -3,8 +3,11 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]))
 
+(defn slurp-day [day]
+  (slurp (io/resource (str "day" day "-input.txt"))))
+
 (defn load-input [day]
-  (s/split-lines (slurp (io/resource (str "day" day "-input.txt")))))
+  (s/split-lines (slurp-day day)))
 
 (defn load-input-int [day]
   (map
